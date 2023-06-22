b711c09a3c2c9991595fd51ef8d49681beb687cd
sadohad.denis <sadoha.denis@gmail.com>
2023-06-22 17:25:56 +0700

    add inoi token for testnet

--- marketkit/src/main/java/io/horizontalsystems/marketkit/syncers/CoinSyncer.kt
+++ marketkit/src/main/java/io/horizontalsystems/marketkit/syncers/CoinSyncer.kt
@@ -99,7 +99,24 @@ class CoinSyncer(
             reference = "0x22FcC36558F0e02aF135045EDB0a43f64511DA59"
         )
 
-        storage.update(coins.plus(inoiCoin), blockchainEntities, tokenEntities.plus(inoiToken))
+        val inoiCoinTest = Coin(
+            uid = "inoiTest",
+            name = "INOI Token",
+            code = "INOI",
+            marketCapRank = -1,
+            coinGeckoId = null
+        )
+        val inoiTokenTest = TokenEntity(
+            coinUid = "inoiTest",
+            blockchainUid = "binance-smart-chain",
+            type = "eip20",
+            decimals = 18,
+            reference = "0x0312b9934b10b0c8d05a641fe5381d4303e4e2ee"
+        )
+
+        val coinsList = listOf(inoiCoin, inoiCoinTest)
+        val tokensList = listOf(inoiToken, inoiTokenTest)
+        storage.update(coins.plus(coinsList), blockchainEntities, tokenEntities.plus(tokensList))
         fullCoinsUpdatedObservable.onNext(Unit)
     }
 
